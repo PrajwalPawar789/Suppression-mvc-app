74eacbf4f46906db781096d61e12994c46447a48
PrajwalPawar789 <prajwalrpawar2001@gmail.com>
2024-06-14 01:55:04 +0530

    We have successfully implemented Master MSFT suppression check + single and all client code master suppression check where you can have multiple entry's on same client_code same left_3, left_4, linkedin_link but different campaign id and different end_client_code (insertdata)

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -256,7 +256,7 @@ async function processFile(filePath, clientCode, dateFilter) {
 }
 
 // Read the Excel file, calculate left_3 and left_4, check the database, and add status
-async function processFileDynamicQuery(filePath, left3, left4, dateFilter) {
+async function processFileDynamicQuery(filePath, dateFilter) {
   const workbook = new ExcelJS.Workbook();
   await workbook.xlsx.readFile(filePath);
   const worksheet = workbook.getWorksheet(1);
@@ -429,11 +429,189 @@ async function processFileDynamicQuery(filePath, left3, left4, dateFilter) {
   return newFilePath;
 }
 
+async function processFileDynamicQueryMSFT(filePath, dateFilter) {
+  const workbook = new ExcelJS.Workbook();
+  await workbook.xlsx.readFile(filePath);
+  const worksheet = workbook.getWorksheet(1);
+
+  // Check if all required column names are present
+  const requiredColumns = [
+    "Company Name",
+    "First Name",
+    "Last Name",
+    "Email ID",
+    "Phone Number",
+    "linkedinLink", // Added linkedinLink as a required column
+  ];
+  const missingColumns = requiredColumns.filter(
+    (colName) => !worksheet.getRow(1).values.includes(colName)
+  );
+  if (missingColumns.length > 0) {
+    return { error: `Missing columns: ${missingColumns.join(", ")}` };
+  }
+
+  // Proceed with processing the file
+  let companyIndex,
+    firstNameIndex,
+    lastNameIndex,
+    emailIndex,
+    phoneIndex,
+    linkedinLinkIndex;
+  const headerRow = worksheet.getRow(1);
+  headerRow.eachCell({ includeEmpty: true }, (cell, colNumber) => {
+    switch (cell.value) {
+      case "Company Name":
+        companyIndex = colNumber;
+        break;
+      case "First Name":
+        firstNameIndex = colNumber;
+        break;
+      case "Last Name":
+        lastNameIndex = colNumber;
+        break;
+      case "Email ID":
+        emailIndex = colNumber;
+        break;
+      case "Phone Number":
+        phoneIndex = colNumber;
+        break;
+      case "linkedinLink":
+        linkedinLinkIndex = colNumber;
+        break;
+    }
+  });
+
+  const dateStatusColumn = worksheet.getColumn(worksheet.columnCount + 1);
+  dateStatusColumn.header = "Date Status";
+
+  const emailStatusColumn = worksheet.getColumn(worksheet.columnCount + 2);
+  emailStatusColumn.header = "Email Status";
+
+  const clientCodeStatusColumn = worksheet.getColumn(worksheet.columnCount + 3);
+  clientCodeStatusColumn.header = "Client Code Status";
+
+  const matchStatusColumn = worksheet.getColumn(worksheet.columnCount + 4);
+  matchStatusColumn.header = "Match Status";
+
+  const linkedinLinkStatusColumn = worksheet.getColumn(worksheet.columnCount + 5);
+  linkedinLinkStatusColumn.header = "LinkedIn Link Status";
+
+  const endClientNameStatusColumn = worksheet.getColumn(worksheet.columnCount + 6);
+  endClientNameStatusColumn.header = "End Client Name Status";
+
+  for (let i = 2; i <= worksheet.rowCount; i++) {
+    const row = worksheet.getRow(i);
+    const firstName = normalizeString(row.getCell(firstNameIndex).value);
+    const lastName = normalizeString(row.getCell(lastNameIndex).value);
+    const companyName = normalizeString(row.getCell(companyIndex).value);
+    const email = normalizeString(row.getCell(emailIndex).value);
+    const linkedinLink = normalizeString(row.getCell(linkedinLinkIndex).value); // Fetch linkedinLink
+
+    const calculatedLeft3 = `${firstName.substring(0, 3)}${lastName.substring(0, 3)}${companyName.substring(0, 3)}`;
+    const calculatedLeft4 = `${firstName.substring(0, 4)}${lastName.substring(0, 4)}${companyName.substring(0, 4)}`;
+
+    // Use the query for client code 'MSFT'
+    const dynamicQueryMSFT = `
+      WITH data AS (
+          SELECT 
+              $1 AS linkedin_link,
+              'TE16' AS client_code,
+              $2 AS left_3,
+              $3 AS left_4,
+              $4 AS email_id,
+              'MSFT' AS end_client_name_1,
+              'Microsoft' AS end_client_name_2  -- Second possible value for end_client_name
+      ),
+      filtered_campaigns AS (
+          SELECT
+              CASE
+                  WHEN (current_date - to_date(c.date_, 'DD-Mon-YY'))::int > $5 THEN 'Suppression Cleared'
+                  ELSE 'Still Suppressed'
+              END AS date_status,
+              CASE
+                  WHEN c.left_3 = d.left_3 AND c.left_4 = d.left_4 THEN 'Match'
+                  ELSE 'Unmatch'
+              END AS match_status,
+              CASE
+                  WHEN c.email = d.email_id THEN 'Match'
+                  ELSE 'Unmatch'
+              END AS email_status,
+              CASE
+                  WHEN c.client = d.client_code THEN 'Match (' || c.client || ')'
+                  ELSE 'Unmatch'
+              END AS client_code_status,
+              CASE
+                  WHEN c.linkedin_link = d.linkedin_link THEN 'Match'
+                  ELSE 'Unmatch'
+              END AS linkedin_link_status,
+              CASE
+                  WHEN c.end_client_name = d.end_client_name_1 THEN 'Match (' || d.end_client_name_1 || ')'
+                  WHEN c.end_client_name = d.end_client_name_2 THEN 'Match (' || d.end_client_name_2 || ')'
+                  ELSE 'Unmatch'
+              END AS end_client_name_status
+          FROM
+              public.campaigns c
+          JOIN
+              data d ON c.client = d.client_code
+          WHERE
+              c.linkedin_link = d.linkedin_link
+      ),
+      final_result AS (
+          SELECT * FROM filtered_campaigns
+          WHERE date_status = 'Still Suppressed'
+          UNION ALL
+          SELECT * FROM filtered_campaigns
+          WHERE date_status = 'Suppression Cleared' AND NOT EXISTS (
+              SELECT 1 FROM filtered_campaigns WHERE date_status = 'Still Suppressed'
+          )
+      )
+      SELECT 
+          COALESCE(date_status, 'Fresh Lead GTG') AS date_status,
+          COALESCE(match_status, 'Unmatch') AS match_status,
+          COALESCE(email_status, 'Unmatch') AS email_status,
+          COALESCE(client_code_status, 'Unmatch') AS client_code_status,
+          COALESCE(linkedin_link_status, 'Unmatch') AS linkedin_link_status,
+          COALESCE(end_client_name_status, 'Unmatch') AS end_client_name_status
+      FROM (
+          SELECT * FROM final_result
+          UNION ALL
+          SELECT 'Fresh Lead GTG' AS date_status, 'Unmatch' AS match_status, 'Unmatch' AS email_status, 'Unmatch' AS client_code_status, 'Unmatch' AS linkedin_link_status, 'Unmatch' AS end_client_name_status
+          WHERE NOT EXISTS (SELECT 1 FROM final_result)
+      ) AS subquery
+      LIMIT 1;
+    `;
+
+    // Execute the dynamic query with parameterized values
+    const dbResult = await pool.query(dynamicQueryMSFT, [
+      linkedinLink,
+      calculatedLeft3,
+      calculatedLeft4,
+      email,
+      dateFilter * 30,
+    ]);
+
+    row.getCell(dateStatusColumn.number).value = dbResult.rows[0].date_status;
+    row.getCell(emailStatusColumn.number).value = dbResult.rows[0].email_status;
+    row.getCell(clientCodeStatusColumn.number).value = dbResult.rows[0].client_code_status;
+    row.getCell(matchStatusColumn.number).value = dbResult.rows[0].match_status;
+    row.getCell(linkedinLinkStatusColumn.number).value = dbResult.rows[0].linkedin_link_status;
+    row.getCell(endClientNameStatusColumn.number).value = dbResult.rows[0].end_client_name_status;
+
+    row.commit();
+  }
+
+  const newFilePath = "Updated-" + Date.now() + ".xlsx";
+  await workbook.xlsx.writeFile(newFilePath);
+  return newFilePath;
+}
+
+
 
 module.exports = {
   upload,
   processFile,
   processFileDynamicQuery,
+  processFileDynamicQueryMSFT,
   uploadFile: async (req, res) => {
     if (!req.file) {
       return res.status(400).send("No file uploaded.");
@@ -454,6 +632,20 @@ module.exports = {
           req.file.path,
           "left_3",
           "left_4",
+          dateFilter
+        );
+        if (result.error) {
+          return res.status(400).send(result.error);
+        }
+        res.download(result, (err) => {
+          if (err) throw err;
+          fs.unlinkSync(result);
+          fs.unlinkSync(req.file.path);
+        });
+      } else if (clientCode === "MSFT") {
+        // Execute the dynamic query for MSFT
+        const result = await processFileDynamicQueryMSFT(
+          req.file.path,
           dateFilter
         );
         if (result.error) {
--- controllers/suppressionDataController.js
+++ controllers/suppressionDataController.js
@@ -7,20 +7,46 @@ const pool = new Pool({
     database: 'supppression-db',
     password: 'root',
     port: 5432
-  });
+});
 
-async function insertSuppressionData(req, res) {
-    const { left3, left4, clientCode, date } = req.body;
+
+async function insertSuppressionData(rowData, index) {
     const client = await pool.connect();
     try {
-        const query = `
-            INSERT INTO campaigns (left_3, left_4, client, date_)
-            VALUES ($1, $2, $3, $4);`;
-        await client.query(query, [left3, left4, clientCode, date]);
-        res.send("Data inserted successfully.");
-    } catch (err) {
-        console.error("Error inserting data:", err);
-        res.status(500).send("Failed to insert data.");
+        const checkQuery = `
+            SELECT 1 FROM campaigns 
+            WHERE client = $1 AND campaign_id = $2 AND end_client_name = $3 
+            AND left_3 = $4 AND left_4 = $5 AND linkedin_link = $6;
+        `;
+        const checkResult = await client.query(checkQuery, [
+            rowData.client, rowData.campaignId, rowData.endClientName, 
+            rowData.left3, rowData.left4, rowData.linkedinLink
+        ]);
+
+        if (checkResult.rows.length === 0) {
+            const insertQuery = `
+                INSERT INTO campaigns (
+                    date_, month_, campaign_id, client, end_client_name, campaign_name,
+                    first_name, last_name, company_name, country, phone, email,
+                    linkedin_link, job_title, employee_size, asset, delivery_spoc,
+                    left_3, left_4, call_disposition, bcl_ops_tl_name, response_date
+                ) VALUES (
+                    $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12,
+                    $13, $14, $15, $16, $17, $18, $19, $20, $21, $22
+                );
+            `;
+            await client.query(insertQuery, [
+                rowData.date, rowData.month, rowData.campaignId, rowData.client, rowData.endClientName, rowData.campaignName,
+                rowData.firstName, rowData.lastName, rowData.companyName, rowData.country, rowData.phone, rowData.email,
+                rowData.linkedinLink, rowData.jobTitle, rowData.employeeSize, rowData.asset, rowData.deliverySpoc,
+                rowData.left3, rowData.left4, rowData.callDisposition, rowData.bclOpsTlName, rowData.responseDate
+            ]);
+            console.log(`Inserted new record for row ${index + 1}: left3: ${rowData.left3}, left4: ${rowData.left4}, client: ${rowData.client}`);
+        } else {
+            console.log(`Duplicate record found for row ${index + 1}: left3: ${rowData.left3}, left4: ${rowData.left4}, client: ${rowData.client}`);
+        }
+    } catch (error) {
+        console.error(`Error processing row ${index + 1}:`, error);
     } finally {
         client.release();
     }
@@ -31,40 +57,41 @@ async function processExcel(req, res) {
         return res.status(400).send('No file uploaded.');
     }
 
-    const { dateCheck } = req.body;
     const path = req.file.path;
     try {
         const rows = await readXlsxFile(path);
         console.log(`Total rows (including header): ${rows.length}`);
-
-        // Get header and data rows
+ 
         const headers = rows[0];
         const dataRows = rows.slice(1);
         console.log(`Processing ${dataRows.length} rows of data.`);
 
-        // Get the index of each column from the headers
         const indexes = {
-            date: headers.indexOf('Date_'),
-            month: headers.indexOf('Month_'),
-            campaignId: headers.indexOf('Campaign_ID'),
-            client: headers.indexOf('Client'),
-            endClientName: headers.indexOf('End_Client_Name'),
-            campaignName: headers.indexOf('Campaign_Name'),
+            date: headers.indexOf('date_'),
+            month: headers.indexOf('month_'),
+            campaignId: headers.indexOf('campaign_id'),
+            client: headers.indexOf('client'),
+            endClientName: headers.indexOf('end_client_name'),
+            campaignName: headers.indexOf('campaign_name'),
             firstName: headers.indexOf('first_name'),
             lastName: headers.indexOf('last_name'),
             companyName: headers.indexOf('company_name'),
             country: headers.indexOf('country'),
             phone: headers.indexOf('phone'),
             email: headers.indexOf('email'),
-            deliverySpoc: headers.indexOf('Delivery_Spoc'),
-            left3: headers.indexOf('Left_3'),
-            left4: headers.indexOf('Left_4'),
-            callDisposition: headers.indexOf('Call Disposition'),
-            bclOpsTlName: headers.indexOf('BCL/Ops TL Name'),
-            category: headers.indexOf('Catagory')
+            linkedinLink: headers.indexOf('linkedin_link'),
+            jobTitle: headers.indexOf('job_title'),
+            employeeSize: headers.indexOf('employee_size'),
+            asset: headers.indexOf('asset'),
+            deliverySpoc: headers.indexOf('delivery_spoc'),
+            left3: headers.indexOf('left_3'),
+            left4: headers.indexOf('left_4'),
+            callDisposition: headers.indexOf('call_disposition'),
+            bclOpsTlName: headers.indexOf('bcl_ops_tl_name'),
+            responseDate: headers.indexOf('response_date')
         };
 
-        const results = await Promise.all(dataRows.map(async (row, index) => {
+        await Promise.all(dataRows.map(async (row, index) => {
             const rowData = {
                 date: row[indexes.date],
                 month: row[indexes.month],
@@ -78,53 +105,20 @@ async function processExcel(req, res) {
                 country: row[indexes.country],
                 phone: row[indexes.phone],
                 email: row[indexes.email],
+                linkedinLink: row[indexes.linkedinLink],
+                jobTitle: row[indexes.jobTitle],
+                employeeSize: row[indexes.employeeSize],
+                asset: row[indexes.asset],
                 deliverySpoc: row[indexes.deliverySpoc],
                 left3: row[indexes.left3],
                 left4: row[indexes.left4],
                 callDisposition: row[indexes.callDisposition],
                 bclOpsTlName: row[indexes.bclOpsTlName],
-                category: row[indexes.category]
+                responseDate: row[indexes.responseDate]
             };
-
-            const client = await pool.connect();
-            try {
-                const query = 'SELECT * FROM campaigns WHERE left_3 = $1 AND left_4 = $2 AND client = $3';
-                const result = await client.query(query, [rowData.left3, rowData.left4, rowData.client]);
-                if (result.rows.length > 0) {
-                    const dbDate = new Date(result.rows[0].date_);
-                    const excelDate = new Date(rowData.date);
-                    const ageDifferenceInMonths = Math.abs(calculateAgeDifferenceInMonths(dbDate, excelDate));
-
-                    if ((dateCheck === '6months' && ageDifferenceInMonths >= 6) || (dateCheck === '1year' && ageDifferenceInMonths >= 12)) {
-                        const updateQuery = 'UPDATE campaigns SET date_ = $1 WHERE left_3 = $2 AND left_4 = $3 AND client = $4';
-                        await client.query(updateQuery, [new Date(), rowData.left3, rowData.left4, rowData.client]);
-                        console.log(`Updated record for row ${index + 1}: left3: ${rowData.left3}, left4: ${rowData.left4}, client: ${rowData.client}`);
-                        return 'exceeds';
-                    } else {
-                        return 'match';
-                    }
-                } else {
-                    const insertQuery = `
-                        INSERT INTO campaigns (date_, month_, campaign_id, client, end_client_name, campaign_name, first_name, last_name, company_name, country, phone, email, delivery_spoc, left_3, left_4, call_disposition, bcl_ops_tl_name, category)
-                        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18)`;
-                    await client.query(insertQuery, [
-                        rowData.date, rowData.month, rowData.campaignId, rowData.client, rowData.endClientName, rowData.campaignName,
-                        rowData.firstName, rowData.lastName, rowData.companyName, rowData.country, rowData.phone, rowData.email,
-                        rowData.deliverySpoc, rowData.left3, rowData.left4, rowData.callDisposition, rowData.bclOpsTlName, rowData.category
-                    ]);
-                    console.log(`Inserted new record for row ${index + 1}: left3: ${rowData.left3}, left4: ${rowData.left4}, client: ${rowData.client}`);
-                    return 'unmatch';
-                }
-            } catch (error) {
-                console.error(`Error processing row ${index + 1}:`, error);
-                return 'error';
-            } finally {
-                client.release();
-            }
+            return insertSuppressionData(rowData, index);
         }));
 
-        const finalResults = results.join(',');
-        console.log(`Final results: ${finalResults}`);
         res.send('Processed successfully. Check server logs for results.');
     } catch (err) {
         console.error('Error processing file:', err);
@@ -132,14 +126,8 @@ async function processExcel(req, res) {
     }
 }
 
-function calculateAgeDifferenceInMonths(date1, date2) {
-    const yearsDiff = date2.getFullYear() - date1.getFullYear();
-    const monthsDiff = date2.getMonth() - date1.getMonth();
-    const totalMonthsDiff = (yearsDiff * 12) + monthsDiff + (date2.getDate() >= date1.getDate() ? 0 : -1);
-    return totalMonthsDiff;
-}
 
 module.exports = {
-    insertSuppressionData,
-    processExcel
+    processExcel,
+    insertSuppressionData
 };
